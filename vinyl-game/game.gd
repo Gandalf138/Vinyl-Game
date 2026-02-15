8a723ca0a9f014a5424741bd3143552d06a3f0e8
Gandalf138 <101952415+Gandalf138@users.noreply.github.com>
2026-02-15 13:55:38 -0800

    Added some buttons
    Added some buttons

--- vinyl-game/game.gd
+++ vinyl-game/game.gd
@@ -1,15 +1,30 @@
 extends Control
 
-@onready var record_button = $CenterContainer/RecordButton
+@onready var record_stack_button = $CenterContainer/CounterContainer/RecordStackButton
+@onready var counter_container = $CenterContainer/CounterContainer
+
+var record_names = ['The Blue Album', 'The White Album', 'Meet the Beatles', 'Led Zeppelin I']
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	create_record_button()
+	create_record_stack_button()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
+func create_record_stack_button():
+	record_stack_button.text = 'Records'
+	record_stack_button.add_theme_font_size_override("font_size", 50)
+	record_stack_button.custom_minimum_size = Vector2(400, 400)
+
+func _on_record_button_pressed() -> void:
+	create_record_button()
+	
 func create_record_button():
-	record_button.text = 'Records'
+	var record_button: Button = Button.new()
+	record_button.text = record_names.pick_random()
+	record_button.add_theme_font_size_override("font_size", 50)
+	record_button.custom_minimum_size = Vector2(400, 400)
+	counter_container.add_child(record_button)
