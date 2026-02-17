925e951cf4ae09fd700cd65ba5be79edc5824ca9
Gandalf138 <spencerclaxton138@gmail.com>
2026-02-17 00:28:41 -0800

    Almost finished maingameplay loop

---
+++ vinyl-game/album_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name AlbumData
+
+@export var artist: String
+@export var album_name: String
+@export var runout: String
+@export var nm_value: int
+@export var cover_texture: Texture2D
--- vinyl-game/game.gd
+++ vinyl-game/game.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@onready var record_container = $CenterContainer/RecordContainer
 @onready var right_counter_container = $RightCenterContainer/RightCounterContainer
 @onready var searchbar = $RightCenterContainer/RightCounterContainer/SearchBar
 @onready var search_label = $RightCenterContainer/RightCounterContainer/SearchLabel
@@ -11,111 +10,119 @@ extends Control
 @onready var Fvalue_label = $RightBottomContainer/ValuesContainer/FValueLabel
 @onready var record_stack = $RecordStack
 @onready var record = $Record
-@onready var album_cover = $Record/Sprite2D
 @onready var vinyl = $Vinyl
+@onready var album_cover = $Record/Sprite2D
 @onready var vinyl_sprite = $Vinyl/Sprite2D
 
-
 var rng = RandomNumberGenerator.new()
-var record_names = ['The Blue Album', 'The White Album', 'Meet the Beatles', 'Led Zeppelin I']
-var quality = ['NM', 'G', 'F']
-var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0',
- '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var albums: Array[AlbumData] = []
+var quality = ['NM', 'VG', 'F']
 var runout = ''
 var record_pressed = true
 var record_present = false
 var album_cover_paths = []
+var nmvalue = 0
+var dragging_record := false
+var drag_offset := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_album_covers()
+	load_albums()
 	create_searchbar()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if dragging_record:
+		var center = vinyl.global_position
+		var mouse_pos = get_global_mouse_position()
+		var angle = center.angle_to_point(mouse_pos)
+		vinyl.rotation = angle + drag_offset
 	
-func load_album_covers():
-	var dir = DirAccess.open("res://art/album covers")
+func load_albums():
+	var dir = DirAccess.open("res://albums")
 	if dir:
 		dir.list_dir_begin()
 		var file = dir.get_next()
 		while file != "":
-			if file.ends_with(".png"):
-				album_cover_paths.append("res://art/album covers/" + file)
+			if file.ends_with(".tres"):
+				albums.append(load("res://albums/" + file))
 			file = dir.get_next()
 	
 func create_searchbar():
-	search_label.text = "Enter nunout text: "
-	search_label.add_theme_font_size_override("font_size", 48)
-	searchbar.add_theme_font_size_override("font_size", 36)
-	searchbar.custom_minimum_size = Vector2(400, 100)
+	search_label.text = "Enter runout text: "
+	search_label.add_theme_font_size_override("font_size", 36)
+	search_label.add_theme_color_override("font_color", Color.BLACK)
+	searchbar.add_theme_font_size_override("font_size", 30)
+	searchbar.custom_minimum_size = Vector2(200, 50)
+
+			
+func create_record():
+	var album = albums.pick_random()
+	album_cover.texture = album.cover_texture
+	runout = album.runout
+	nmvalue = album.nm_value
 	
-func create_record_button():
-	for child in record_container.get_children():
-		if child is Button:
-			child.queue_free()
-		
-	var record_button: Button = Button.new()
-	record_button.text = record_names.pick_random()
-	record_button.add_theme_font_size_override("font_size", 48)
-	record_button.custom_minimum_size = Vector2(400, 400)
-	record_button.pressed.connect(_on_record_button_pressed.bind())
-	record_container.add_child(record_button)
-
-func _on_record_button_pressed():
-	if record_pressed == false:
-		runout = get_runout()
-		quality_label.text = "Quality: " + quality.pick_random()
-		quality_label.add_theme_font_size_override("font_size", 48)
-		runout_label.text = 'Runout Text: ' + runout
-		runout_label.add_theme_font_size_override("font_size", 48)
-	
-	record_pressed = true
-	
-func get_runout():
-	var text  = ''
-	for i in range(5):
-		text += characters.pick_random()
-	return text
-
-func _on_search_bar_text_submitted(new_text: String) -> void:
-	var nmvalue = int(rng.randi_range(70, 100))
-	var vgvalue = int(floor(nmvalue * .7))
-	var fvalue = int(floor(nmvalue * .2))
-
-	if new_text == runout:
-		NMvalue_label.text = 'NM: $' + str(nmvalue)
-		NMvalue_label.add_theme_font_size_override("font_size", 48)
-		VGvalue_label.text = 'VG: $' + str(vgvalue)
-		VGvalue_label.add_theme_font_size_override("font_size", 48)
-		Fvalue_label.text = 'F: $' + str(fvalue)
-		Fvalue_label.add_theme_font_size_override("font_size", 48)
-
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			dragging_record = false
+			
 func _on_record_stack_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			quality_label.text = ''
+			runout_label.text = ''
+			runout = ''
+			vinyl_sprite.texture = null
 			record_pressed = false
 			record_present = true
 			create_record()
-			
-func create_record():
-	var random_path = album_cover_paths.pick_random()
-	var texture = load(random_path)
-	album_cover.texture = texture
 
 func _on_record_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if record_present == true:
 				if record_pressed == false:
-					var vinyl = load("res://art/vinyl.png")
-					vinyl_sprite.texture = vinyl
-					#runout = get_runout()
-					#quality_label.text = "Quality: " + quality.pick_random()
-					#quality_label.add_theme_font_size_override("font_size", 48)
-					#runout_label.text = 'Runout Text: ' + runout
-					#runout_label.add_theme_font_size_override("font_size", 48)
+					vinyl_sprite.texture = load("res://art/vinyl.png")
+					quality_label.text = "Quality: " + quality.pick_random()
+					quality_label.add_theme_font_size_override("font_size", 36)
+					runout_label.text = 'Runout Text: ' + runout
+					runout_label.add_theme_font_size_override("font_size", 36)
 				
 				record_pressed = true
+				
+func _on_vinyl_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				var mouse_pos = get_global_mouse_position()
+				var center_x = vinyl.global_position.x
+
+				var clicked_right_side = mouse_pos.x > center_x
+
+				if clicked_right_side:
+					dragging_record = true
+					
+					var center = vinyl.global_position
+					
+					var click_angle = center.angle_to_point(mouse_pos)
+					drag_offset = vinyl.rotation - click_angle
+
+func _on_search_bar_text_submitted(new_text: String) -> void:
+	var vgvalue = int(floor(nmvalue * .7))
+	var fvalue = int(floor(nmvalue * .2))
+
+	if new_text == runout and runout != '':
+		NMvalue_label.text = 'NM: $' + str(nmvalue)
+		NMvalue_label.add_theme_font_size_override("font_size", 24)
+		NMvalue_label.add_theme_color_override("font_color", Color.BLACK)
+		VGvalue_label.text = 'VG: $' + str(vgvalue)
+		VGvalue_label.add_theme_font_size_override("font_size", 24)
+		VGvalue_label.add_theme_color_override("font_color", Color.BLACK)
+		Fvalue_label.text = 'F: $' + str(fvalue)
+		Fvalue_label.add_theme_font_size_override("font_size", 24)
+		Fvalue_label.add_theme_color_override("font_color", Color.BLACK)
+	elif runout == '':
+		NMvalue_label.text = ''
+		VGvalue_label.text = ''
+		Fvalue_label.text = ''
